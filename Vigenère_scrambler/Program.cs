54575240ae5e5304c061618ad52dae7ead5547ac
aerosmith_dp <aerosmith.dp@gmail.com>
2021-12-15 01:30:36 +0200

    first commit

---
+++ Vigenère_scrambler/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vigenère_scrambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the value you want to encrypt(only latin letters a-z): ");
+            string valueForEncrypt = Console.ReadLine().ToLower();
+            Console.Write("Enter a key(only latin letters a-z): ");
+            string key = Console.ReadLine().ToLower();
+
+            int counter = 0;
+            if (key.Length < valueForEncrypt.Length)
+            {
+                for (int i = key.Length; i < valueForEncrypt.Length; i++)
+                {
+                    key += key[counter];
+                    counter++;
+                    if (counter == key.Length)
+                    {
+                        counter = 0;
+                    }
+                }
+            }
+
+            if (key.Length > valueForEncrypt.Length)
+            {
+                key = key.Substring(0, valueForEncrypt.Length);
+            }
+
+            string encryptedValue = VigenèreСipher.Encrypt(valueForEncrypt, key);
+            Console.Write($"\n\tHere is what you entered encrypted: {encryptedValue}\n");
+        }
+
+    }
+}
---
+++ Vigenère_scrambler/VigenèreСipher.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace Vigenère_scrambler
+{
+    class VigenèreСipher
+    {
+        public static string Encrypt(string valueForEncrypt, string key)
+        {
+            string result = "";
+
+            for (int i = 0; i < valueForEncrypt.Length; i++)
+            {
+                int encryptKey = Dictionaries.dictionaryReverse[valueForEncrypt[i]] + Dictionaries.dictionaryReverse[key[i]];
+                if (encryptKey > 25)
+                {
+                    encryptKey -= 26;
+                }
+                result = result + Dictionaries.dictionary[encryptKey];
+            }
+            return result;
+        }
+
+    }
+}
